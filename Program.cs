a5532e59dd1ca828536ace73bc48bebcba215937
Brian Davison <b.davison@napier.ac.uk>
2024-10-18 09:33:22 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void DisplayDate()
     {
-        string date = DateTime.Now.ToString("D");
+        string date = DateTime.Now.ToString("dd/MM/yyyy"); // Format: DD/MM/YYYY
         Console.WriteLine($"Today's date is {date}");
     }
 }
